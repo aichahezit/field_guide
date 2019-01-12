a9ca833cc86575f40247fda88ba206fa83ba557c
aichahezit <aichez18@hotmail.com>
2019-01-11 20:19:38 -0800

    Navigation bar icons added

--- app/src/main/java/com/example/aicha/fieldguide/MainActivity.java
+++ app/src/main/java/com/example/aicha/fieldguide/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_location:
+                    mTextMessage.setText(R.string.title_location);
+                    return true;
+                case R.id.navigation_add:
+                    mTextMessage.setText(R.string.title_add);
                     return true;
             }
             return false;
